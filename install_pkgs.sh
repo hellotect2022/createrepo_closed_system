a1ff6e7d6cef08595eca0188ae680ad29f0cb964
dhhan <sd>
2023-09-11 10:14:16 +0900

    경로수정

--- install_pkgs.sh
+++ install_pkgs.sh
@@ -5,6 +5,6 @@ cd "$(pwd)"
 createrepo .
 
 
-echo -e "[dh_repo]\nname=dh_repo\nbaseurl=file://"$(pwd)"\ngpgcheck=0\nenabled=1" 1> /etc/yum.repos.d/dh_repo.repo
+echo -e "[dh_repo]\nname=dh_repo\nbaseurl=file://"$(pwd)"/pkgs\ngpgcheck=0\nenabled=1" 1> /etc/yum.repos.d/dh_repo.repo
 
 yum install -y screen htop tar createrepo zip unzip vim --disablerepo=* --enablerepo=dh_repo
